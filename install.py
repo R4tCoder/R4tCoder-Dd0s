1d373592335006a7133645b715f453091f0f234a
R4tCoder <semraerkus6@gmail.com>
2025-01-02 21:09:37 +0300

    Create install.py

---
+++ install.py
@@ -0,0 +1,10 @@
+from os import system
+
+system('pip install requests')
+system('pip install pyfiglet')
+system('pip install argparse')
+system('pip install concurrent.futures')
+system('pip install termcolor')
+system('pip install colorama')
+system('pip install socket')
+print('installed')
